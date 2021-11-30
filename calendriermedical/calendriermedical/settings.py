cd2eeb91243abd12deda0d2b42cbc3f6a4d89611
YannickAiguier <yannick.aiguier@gmail.com>
2021-11-30 11:24:33 +0100

    config: timezone and database settings

--- calendriermedical/calendriermedical/settings.py
+++ calendriermedical/calendriermedical/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'calendriermedical.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'database': 'calendrier_medical',
+            'user': 'dr_raoult',
+            'password': 'coronavirus',
+            'default-character-set': 'utf8'
+        }
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
