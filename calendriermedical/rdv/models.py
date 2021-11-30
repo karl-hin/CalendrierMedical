74c9f1272aec7d2d67ee57d800e279941bfedd5e
YannickAiguier <yannick.aiguier@gmail.com>
2021-11-30 13:56:16 +0100

    Dev: User model extension

--- calendriermedical/rdv/models.py
+++ calendriermedical/rdv/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 from enum import Enum
+# for User model extension
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class TypeChoice(Enum):
-    SIMPLE = "simple"
-    SPECIALIST = "specialiste"
-    MANIPULATION = "manipulation"
+class Group(Enum):
+    PATIENT = "patient"
+    DOCTOR = "doctor"
 
 
-class Patient:
-    pass
+class Profile(models.Models):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    group = models.CharField(choices=[(tag, tag.value) for tag in Group])
 
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
-class Doctor:
-    pass
-
-
-class Rdv(models.Model):
-    date = models.DateField()
-    hours = models.TimeField()
-    type = models.CharField(choices=[(tag, tag.value) for tag in TypeChoice])
-    doctor = models.ForeignKey(Doctor)
-    patient = models.ForeignKey(Patient)
+    @receiver()(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
