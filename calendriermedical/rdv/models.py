fa57b6a19905b22c9a5c775eb9e9be52b283de2a
YannickAiguier <yannick.aiguier@gmail.com>
2021-11-30 16:15:30 +0100

    Dev: PatientProfile and DoctorProfile

--- calendriermedical/rdv/models.py
+++ calendriermedical/rdv/models.py
@@ -1,26 +1,36 @@
 from django.db import models
 from enum import Enum
 # for User model extension
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-class Group(Enum):
-    PATIENT = "patient"
-    DOCTOR = "doctor"
+class User(AbstractUser):
+    is_patient = models.BooleanField(default=True)
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    group = models.CharField(max_length=7,
-                             choices=[(tag, tag.value) for tag in Group])
+class PatientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True,
+                                related_name='patient_profile')
 
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
 
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
+class DoctorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True,
+                                related_name='doctor_profile')
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if instance.is_patient:
+        PatientProfile.objects.get_or_create(user=instance)
+    else:
+        DoctorProfile.objects.get_or_create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    if instance.is_patient:
+        instance.patient_profile.save()
+    else:
+        instance.doctor_profile.save()
