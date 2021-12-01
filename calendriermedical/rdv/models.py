1b160c80fdd94f0f233417e2c5ac27b0287b7570
YannickAiguier <yannick.aiguier@gmail.com>
2021-12-01 11:47:34 +0100

    Dev: functions to list available slots

--- calendriermedical/rdv/models.py
+++ calendriermedical/rdv/models.py
@@ -1,3 +1,5 @@
+import time
+
 from django.db import models
 from enum import Enum
 # for User model extension
@@ -49,5 +51,46 @@ class Rdv(models.Model):
     # doctor = models.ForeignKey(Doctor)
     # patient = models.ForeignKey(Patient)
 
+    def end(self):
+        if self.type == "simple":
+            delta = time(0, 30)
+        elif self.type == "specialiste":
+            delta = time(0, 45)
+        else:
+            delta = time(0, 55)
+        return self.hours + delta
+
     def __str__(self):
         return self.date + self.hours + self.type
+
+
+# utils functions
+def get_available_slots(date):
+    rdvs = get_rdv_date(date)
+    available_slots = get_daily_slots(date)
+    for rdv in rdvs:
+        deactivate_slots(available_slots, rdv)
+    return available_slots
+
+
+def get_rdv_date(date):
+    return Rdv.objects.get(date=date)
+
+
+def get_daily_slots(date):
+    if date.weekday() <= 3:
+        slots = ['8:00', '8:15', '8:30', '8:45', '9:00', '9:15', '9:30', '9:45',
+                 '10:00', '10:15', '10:30', '10:45', '11:00', '11:15', '11:30',
+                 '14:00', '14:15', '14:30', '14:45', '15:00', '15:15', '15:30', '15:45',
+                 '16:00', '16:15', '16:30', '16:45', '17:00', '17:15', '17:30']
+    else:
+        slots = ['14:00', '14:15', '14:30', '14:45', '15:00', '15:15', '15:30', '15:45',
+                 '16:00', '16:15', '16:30']
+    for slot in slots:
+        slot = time.strptime(slot, '%I:%M')
+    return slots
+
+
+def deactivate_slots(available_slots, rdv):
+    available_slots = [slot for slot in available_slots if slot == rdv.hours
+                       or slot < rdv.end()]
